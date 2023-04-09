931cda339c35b73471df8d8fe6cd7df1334c9bd4
EtriKodoku <volodymyrpetriv2207@gmail.com>
2023-04-09 20:41:05 +0300

    Linked static files

--- mt/math_test/urls.py
+++ mt/math_test/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import hello
 
 urlpatterns = [
-    path('', pass, name="Main"),
-]
+    path('', hello, name="Main"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
