f462185cbe6e533e8c3e9de750ecb029c2863267
Igor4er <sherednihenko.ihor@gmail.com>
2023-04-09 15:27:07 +0300

    type 1 eqs

--- mt/math_test/services/eqgen.py
+++ mt/math_test/services/eqgen.py
@@ -1,2 +1,23 @@
+import random
+
+def gs():
+    return random.choice(list(range(-7, -1)) + list(range(1, 7)))
+
+def dl_1():
+    b = gs()
+    c = gs()
+
+    v1 = b + c
+    v2 = b * c
+
+    return {
+        "type": 1,
+        "a": 1,
+        "b": b,
+        "c": c
+    }
+
+
 if __name__ == "__main__":
-    pass
+    eq = dl_1()
+    print(f"{eq['a'] if eq['a'] != 1 else ''}x^2 {'+' if eq['b'] > 0 else '-'} {eq['b']} {'+' if eq['c'] > 0 else '-'} {eq['c']}")
