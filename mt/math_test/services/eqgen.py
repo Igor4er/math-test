6fd695b47542fef4a69d7cf26dd89e4e7a4379c7
EtriKodoku <volodymyrpetriv2207@gmail.com>
2023-04-09 15:46:35 +0300

    Added difficulty 3

--- mt/math_test/services/eqgen.py
+++ mt/math_test/services/eqgen.py
@@ -1,7 +1,10 @@
 import random
+from math import sqrt
+
 
 def gs():
     return random.choice(list(range(-7, -1)) + list(range(1, 7)))
+
 
 def dl_1():
     b = gs()
@@ -18,6 +21,30 @@ def dl_1():
     }
 
 
+def dl_3():
+    v1 = gs()
+    v2 = gs()
+    a = gs()
+
+    b = -(v1+v2)
+    c = v1*v2
+
+    D = b**2 - 4*a*c
+
+    if D >= 0:
+        if not sqrt(D).is_integer():
+            return dl_3()
+        else:
+            return {
+                "type": 3,
+                "a": a,
+                "b": b,
+                "c": c
+            }
+    else:
+        return dl_3()
+
+
 if __name__ == "__main__":
-    eq = dl_1()
-    print(f"{eq['a'] if eq['a'] != 1 else ''}x^2 {'+' if eq['b'] > 0 else '-'} {abs(eq['b'])} {'+' if eq['c'] > 0 else '-'} {abs(eq['c'])}")
+    eq = dl_3()
+    print(f"{eq['a'] if eq['a'] != 1 else ''}x^2 {'+' if eq['b'] > 0 else '-'} {abs(eq['b'])} {'+' if eq['c'] > 0 else '-'} {abs(eq['c'])} = 0")
